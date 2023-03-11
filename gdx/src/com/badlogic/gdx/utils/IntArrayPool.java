a5434cf181fe720915bd290c570c769324ab165c
Yahel Cohen <yahelcohenko@gmail.com>
2023-03-11 12:01:42 +0200

    Added Apache file header to IntArrayPool

--- gdx/src/com/badlogic/gdx/utils/IntArrayPool.java
+++ gdx/src/com/badlogic/gdx/utils/IntArrayPool.java
@@ -1,8 +1,23 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 
 package com.badlogic.gdx.utils;
 
 /** A pool for int[] with a fixed size of {@link IntArrayPool#arraySize}. */
-public class IntArrayPool extends Pool {
+public class IntArrayPool extends Pool<int[]> {
 	private int arraySize;
 
 	public IntArrayPool (int arraySize) {
@@ -14,7 +29,7 @@ public class IntArrayPool extends Pool {
 	}
 
 	@Override
-	protected Object newObject () {
+	protected int[] newObject () {
 		return new int[arraySize];
 	}
 }
